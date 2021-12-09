c643d6700e88218bbcdb6839f86183fb76262f37
Arnab Bhattacharya <Arnab.Bhattacharya@maersk.com>
2021-12-09 12:40:17 +0530

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace problemdetailscheck.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Consumes("application/json")]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+        public IActionResult Get()
+        {
+            var validationFailures =
+                new[]
+                {
+                    new ValidationFailure("Property1", "lorem ipsum dolor") { Severity = Severity.Warning },
+                    new ValidationFailure("Property2", "lorem ipsum dolor2") { Severity = Severity.Error },
+                };
+            var validationResult = new ValidationResult(validationFailures);
+            var modelState = new Microsoft.AspNetCore.Mvc.ModelBinding.ModelStateDictionary();
+            validationResult.AddToModelState(modelState, null);
+            return BadRequest(new ValidationProblemDetails(modelState));
+        }
+    }
+}
